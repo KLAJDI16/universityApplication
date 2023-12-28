655e2a9498ffea408f32e043122bfeb6ba0b9d65
Erta Zogu <erta.zogu8@gmail.com>
2023-12-28 10:24:46 +0100

    log-in page

--- university-app-interface/src/app/api.service.ts
+++ university-app-interface/src/app/api.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  authenticate(data: any): Observable<any> {
+    // Assuming your authentication endpoint is 'your-authentication-endpoint'
+    return this.httpClient.post('your-authentication-endpoint', data);
+  }
 }
+
--- university-app-interface/src/app/log-in/log-in.component.ts
+++ university-app-interface/src/app/log-in/log-in.component.ts
@@ -1,12 +1,62 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from '../api.service';
+
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule, ReactiveFormsModule,
+    MatFormFieldModule, MatInputModule, MatButtonModule, RouterModule, HttpClientModule],
   templateUrl: './log-in.component.html',
-  styleUrl: './log-in.component.css'
+  styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent {
+  loginForm!: FormGroup;
 
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar, public apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onLogin() {
+    if (!this.loginForm.valid){
+      this.openSnackBar();
+    }
+    else {
+      this.login();
+    }
+    console.log(this.loginForm.value);
+  }
+  login() {
+    //fullName,
+    const { username, password } = this.loginForm.value;
+    const data = { username, password };
+
+    this.apiService.authenticate(data).subscribe(
+      (response:any)  => {
+      console.log('Login response:', response);
+    });
+  }
+
+
+  openSnackBar() {
+      this.snackBar.open('Please fill in all required fields.', 'Close', {
+        duration: 2000,
+      });
+  }
+  
 }
